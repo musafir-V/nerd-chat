ce547a28d4df431bab051b5370f188d203b019fc
Shivang Awasthi <codershiwang.awasthi@gmail.com>
2020-09-25 00:46:38 +0530

    Updates

---
+++ precache-manifest.a7cfa558bce615bcc1557e08fa357298.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aac717b0820fcd6023a2b143e6d9bd3",
+    "url": "/nerd-chat/index.html"
+  },
+  {
+    "revision": "575d64ac1eeb674f366f",
+    "url": "/nerd-chat/static/css/2.89f0d7c1.chunk.css"
+  },
+  {
+    "revision": "451ea82043222ca7989e",
+    "url": "/nerd-chat/static/css/main.a37e892f.chunk.css"
+  },
+  {
+    "revision": "575d64ac1eeb674f366f",
+    "url": "/nerd-chat/static/js/2.e9af0c50.chunk.js"
+  },
+  {
+    "revision": "fb38d244761f3aee0c2a13e6bc129471",
+    "url": "/nerd-chat/static/js/2.e9af0c50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "451ea82043222ca7989e",
+    "url": "/nerd-chat/static/js/main.a263e842.chunk.js"
+  },
+  {
+    "revision": "3d19298d5256c72efe70",
+    "url": "/nerd-chat/static/js/runtime-main.c63b5941.js"
+  },
+  {
+    "revision": "f071bb80d93e54a29b817a6ea194fb76",
+    "url": "/nerd-chat/static/media/RegisterBackground.f071bb80.jpg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/nerd-chat/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/nerd-chat/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/nerd-chat/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/nerd-chat/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/nerd-chat/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/nerd-chat/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/nerd-chat/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/nerd-chat/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/nerd-chat/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/nerd-chat/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/nerd-chat/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "81a521427709da3d3d651fac0e429865",
+    "url": "/nerd-chat/static/media/nerd1.81a52142.jpg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/nerd-chat/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/nerd-chat/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/nerd-chat/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/nerd-chat/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/nerd-chat/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
